e50ee0dfe44eb4afe2502220fec709cf09c08f43
saitoyoshi <mitti180@gmail.com>
2023-08-13 19:41:05 +0900

    ロゴを変更

--- resources/views/layouts/navigation.blade.php
+++ resources/views/layouts/navigation.blade.php
@@ -5,8 +5,8 @@
             <div class="flex">
                 <!-- Logo -->
                 <div class="shrink-0 flex items-center">
-                    <a href="{{ route('dashboard') }}">
-                        <x-application-logo class="block h-9 w-auto fill-current text-gray-800 dark:text-gray-200" />
+                    <a href="{{ route('post.index') }}">
+                        <img src="{{ asset('logo/logo_yotuba04.png') }}" alt="" srcset="" height="80" width="80">
                     </a>
                 </div>
 
