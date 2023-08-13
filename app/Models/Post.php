95b5ad3fddb3ea09fe2412effbe461b3c8336c39
saitoyoshi <mitti180@gmail.com>
2023-08-13 15:30:27 +0900

    投稿を登録する処理を実装

--- app/Models/Post.php
+++ app/Models/Post.php
@@ -9,6 +9,12 @@ use Illuminate\Database\Eloquent\Relations\BelongsTo;
 class Post extends Model
 {
     use HasFactory;
+    protected $fillable = [
+        'title',
+        'body',
+        'user_id',
+        'image',
+    ];
     public function user(): BelongsTo {
         return $this->belongsTo(User::class);
     }
