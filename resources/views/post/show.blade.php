e3e60b55494fbc81b321d721e6824cad5a633674
saitoyoshi <mitti180@gmail.com>
2023-08-15 06:37:32 +0900

    削除処理を実装

--- resources/views/post/show.blade.php
+++ resources/views/post/show.blade.php
@@ -13,12 +13,20 @@
           <div class="px-10 mt-4">
 
               <div class="bg-white w-full  rounded-2xl px-10 py-8 shadow-lg hover:shadow-2xl transition duration-500">
-                  <div class="mt-4">
-                      <h1 class="text-lg text-gray-700 font-semibold">
-                          {{ $post->title }}
-                      </h1>
-                      <a href="{{ route('post.edit', $post) }}"><x-primary-button class="bg-teal-700 float-right">編集</x-primary-button></a>
-                      <hr class="w-full">
+                <div class="mt-4">
+                    <h1 class="text-lg text-gray-700 font-semibold">
+                        {{ $post->title }}
+                    </h1>
+                    <hr class="w-full">
+                </div>
+                <div class="flex justify-end mt-4">
+                    <a href="{{route('post.edit', $post)}}"><x-primary-button class="bg-teal-700 float-right">編集</x-primary-button></a>
+                    <form method="post" action="{{route('post.destroy', $post)}}">
+                    @csrf
+                    @method('delete')
+                        <x-primary-button class="bg-red-700 float-right ml-4" onClick="return confirm('本当に削除しますか？');">削除</x-primary-button>
+                    </form>
+                </div>
                       <p class="mt-4 text-gray-600 py-4 whitespace-pre-line">{{$post->body}}</p>
                       @if($post->image)
 
